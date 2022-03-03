273105a786e6e69ee0dd35278932e95c9ceb2cda
principle105 <principle105@gmail.com>
2022-03-03 00:09:32 -0500

    forgot to remove testing

--- achievements/endurance.py
+++ achievements/endurance.py
@@ -18,22 +18,20 @@ class Endurance(Achievement):
         return self.changer if user[self.key] >= self.value else False
 
     def progress(self, user):
-        return user[self.key], max(self.value, 1)
-        # TODO: remove the 1
+        return user[self.key], self.value
 
 
 def generate_endurance(name, key, values, desc):
     return [Endurance(name, desc.format(value), key, value) for value in values]
 
 
-# TODO: change first value back to 1
 endurance = Category(
     desc="",
     challenges=[
         generate_endurance(
             "Streakin'",
             "streak",
-            (0, 5, 10, 25, 50, 75, 100, 150, 200, 365),
+            (1, 5, 10, 25, 50, 75, 100, 150, 200, 365),
             "Play wordPractice for {} day(s) in a row",
         ),
         generate_endurance(
