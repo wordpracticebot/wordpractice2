d3f17187d346069e96bb28063125cf2941107f72
principle105 <principle105@gmail.com>
2022-06-15 13:22:18 -0400

    added max connections

--- cogs/utils/redis.py
+++ cogs/utils/redis.py
@@ -1,5 +1,5 @@
-import aioredis
 from discord.ext import commands
+from redis import asyncio as aioredis
 
 from config import REDIS_URL
 
@@ -12,7 +12,9 @@ class Redis(commands.Cog):
         self._connect_task = self.bot.loop.create_task(self.connect())
 
     async def connect(self):
-        self.pool = await aioredis.from_url(REDIS_URL)
+        self.pool = await aioredis.from_url(
+            REDIS_URL, socket_timeout=10, max_connections=25
+        )
 
     async def wait_until_ready(self):
         await self._connect_task
