c7c82f1954ca33024bf70390255c8f5abf137d9f
principle105 <principle105@gmail.com>
2023-08-11 22:05:09 -0400

    fixing two words in one

--- cogs/discord/typing.py
+++ cogs/discord/typing.py
@@ -1735,7 +1735,9 @@ class Typing(commands.Cog):
 
         words, wrap = _load_test_file(word_list.languages[user.language][user.level])
 
-        return random.sample(words, length), wrap
+        raw_words = " ".join(random.sample(words, length), wrap)
+
+        return raw_words.split(" ")[:length]
 
     @staticmethod
     async def handle_quote_input(length: str):
